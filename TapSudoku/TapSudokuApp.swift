b6de26ed8e45f3711c49c66d1786c0274f82279c
Mark Townsend <markltownsend@users.noreply.github.com>
2022-12-18 09:30:05 -0800

    Initial Commit

---
+++ TapSudoku/TapSudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapSudokuApp.swift
+//  TapSudoku
+//
+//  Created by Mark Townsend on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TapSudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
